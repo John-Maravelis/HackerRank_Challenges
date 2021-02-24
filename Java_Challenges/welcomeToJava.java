c65b0560320e45a418c9451020d6ca927b023113
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-02-24 21:42:16 +0200

    Simple Hello, World program

---
+++ Java_Challenges/welcomeToJava.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+/**
+* Task: Print to stdout "Hello, World." followed by "Hello, Java." in a new line.
+*/
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
