5842aae2a095798f8bbfd28263d55953d0718682
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-02-25 20:11:47 +0200

    Simple example of a for loop

---
+++ Python_Challenges/forLoop.py
@@ -0,0 +1,11 @@
+"""
+Task: The provided code stub reads and integer, n, from STDIN.
+		For all non-negative integers i<n, print i^2
+"""
+
+# This is needed for the program to pass the automated tests on HackerRank
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(0,n):
+    print(i**2)
