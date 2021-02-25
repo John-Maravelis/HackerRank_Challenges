f52779473c6811dafe7ffd660493e900c6d6ab69
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-02-25 21:12:39 +0200

    Given a number n, calculate the factorial(n)

---
+++ 30DaysOfCode/Day9_Challenge.py
@@ -0,0 +1,23 @@
+"""
+Task: Given a number n, calculate the factorial(n).
+"""
+
+# This is needed for the program to pass the automated tests on HackerRank
+import os
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+def factorial(n):
+	if n > 1:
+		return n * factorial(n - 1)
+	else:
+		return n
