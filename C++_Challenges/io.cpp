bf0a8c961bb17a4c8b946ef1350e136f8c74fd38
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-02-25 19:51:28 +0200

    Simple example of I/O

---
+++ C++_Challenges/io.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/**
+* Task: Read 3 numbers from stdin and print their sum to stdout.
+*/
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c; 
+    cout << a + b + c << endl;
+    return 0;
+}
